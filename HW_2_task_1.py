d077418b0638a453ee4a43ef1bab0d6c529043ed
NRinat <79082803+NRinat@users.noreply.github.com>
2021-04-09 22:56:11 +0600

    Add files via upload
    1.	Выяснить тип результата выражений:
    ●	15 * 3
    ●	15 / 3
    ●	15 // 2
    ●	15 ** 2

---
+++ HW_2_task_1.py
@@ -0,0 +1,19 @@
+a = 15 * 3
+b = 15 / 3
+c = 15 // 2
+d = 15 ** 2
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+print(isinstance(a, int))
+print(isinstance(b, float))
+print(isinstance(c, int))
+print(isinstance(d, int))
+
+print(type(a) == int)
+print(type(b) == float)
+print(type(c) == int)
+print(type(d) == int)
